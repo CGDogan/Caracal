785a81545e58b4fc4ca616dddf345c9da609f227
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-09-10 19:07:04 +0100

    Update dataHandlers.js

--- handlers/dataHandlers.js
+++ handlers/dataHandlers.js
@@ -385,7 +385,7 @@ FSChanged.added = function(req, res, next) {
   res.send(hello);
 }
 
-FSChanged.removed = function(req, res, next) {
+FSChangded.removed = function(req, res, next) {
   var query = req.query;
 }
 
